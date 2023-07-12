eb3327d21dafe5224b468d4174c85469ccaa08a9
vbuttini <vbuttini22@gmail.com>
2023-07-11 21:21:09 -0300

    velocity configuration

---
+++ src/main/java/com/vbuttini/demo/shoe/config/SpringVelocityViewResolver.java
@@ -0,0 +1,124 @@
+package com.vbuttini.demo.shoe.config;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.context.Context;
+import org.apache.velocity.tools.view.VelocityView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+import java.util.Map;
+
+public class SpringVelocityViewResolver implements ViewResolver {
+
+    private final VelocityEngine velocityEngine;
+
+    private final ServletConfig servletConfig;
+
+    private boolean cache;
+    private String prefix;
+    private String suffix;
+    private String contentType;
+
+    public SpringVelocityViewResolver(final VelocityEngine velocityEngine,final ServletConfig servletConfig) {
+        this.velocityEngine = velocityEngine;
+        this.servletConfig = servletConfig;
+    }
+
+    @Override
+    public View resolveViewName(final String viewName,final Locale locale) throws Exception {
+        final VelocityView velocityView = new VelocityView(servletConfig);
+        velocityView.setVelocityEngine(this.velocityEngine);
+        return new SpringVelocityView(velocityView, viewName, locale, cache, prefix, suffix, contentType);
+    }
+
+    public void setPrefix(final String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setSuffix(final String suffix) {
+        this.suffix = suffix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setContentType(final String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setCache(boolean cache) {
+        this.cache = cache;
+    }
+
+    class SpringVelocityView implements View {
+
+        private final VelocityView velocityView;
+        private final String viewName;
+        private final Locale locale;
+
+        private final boolean cache;
+        private final String prefix;
+        private final String suffix;
+        private final String contentType;
+
+        private Template template;
+
+        public SpringVelocityView(final VelocityView velocityView,final String viewName,final Locale locale,final boolean cache, final String prefix,final String suffix,final String contentType) {
+            this.velocityView = velocityView;
+            this.viewName = viewName;
+            this.locale = locale;
+            this.cache = cache;
+            this.prefix = prefix;
+            this.suffix = suffix;
+            this.contentType = contentType;
+        }
+
+        @Override
+        public String getContentType() {
+            return contentType;
+        }
+
+        @Override
+        public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+            final Context context = this.velocityView.createContext(request, response);
+            context.put("locale", locale);
+            for (Map.Entry<String, ?> entry : model.entrySet()){
+                context.put(entry.getKey(), entry.getValue());
+            }
+            // get the template
+            if(template == null || !this.isCache()) {
+                template = this.velocityView.getTemplate(this.prefix + this.viewName + this.suffix);
+            }
+
+            // merge the template and context into the response
+            this.velocityView.merge(template, context, response.getWriter());
+        }
+
+        public String getPrefix() {
+            return prefix;
+        }
+
+        public String getSuffix() {
+            return suffix;
+        }
+
+        public boolean isCache() {
+            return cache;
+        }
+    }
+
+}
---
+++ src/main/java/com/vbuttini/demo/shoe/config/VelocityConfig.java
@@ -0,0 +1,40 @@
+package com.vbuttini.demo.shoe.config;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.ServletConfig;
+
+@Configuration
+public class VelocityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Bean
+    public VelocityEngine velocityEngine() {
+        VelocityEngine velocityEngine = new VelocityEngine();
+        velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADERS, "class");
+        velocityEngine.setProperty("resource.loader.class.class", ClasspathResourceLoader.class.getName());
+        return velocityEngine;
+    }
+
+    @Bean
+    public ViewResolver viewResolver(final VelocityEngine velocityEngine,final ServletConfig servletConfig) {
+        SpringVelocityViewResolver viewResolver = new SpringVelocityViewResolver(velocityEngine, servletConfig);
+        viewResolver.setCache(true);
+        viewResolver.setPrefix("/templates/");
+        viewResolver.setSuffix(".vm");
+        viewResolver.setContentType("text/html;charset=UTF-8");
+        return viewResolver;
+    }
+
+}
